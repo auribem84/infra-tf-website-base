192b7bdc2f56d3d402babbeed0707aa5526e0b28
Arturo Uribe <arturo.uribe@trueaccord.com>
2025-02-23 13:38:56 -0500

    fix: fix typo

--- main.tf
+++ main.tf
@@ -142,7 +142,7 @@ resource "aws_security_group_rule" "allow_alb_all_outbound"{
 }
 
 resource "aws_lb" "load_balancer" {
-    name            = "web-app-lb"
+    name                = "web-app-lb"
     load_balancer_type  =   "application"
     subnets               = data.aws_subnet_ids.default_subnet.ids
     security_groups     = [aws_security_group.alb.id]
